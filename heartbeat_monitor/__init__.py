eca385444589d6cc133c514d7ec258314c082348
github username <annacarey@icloud.com>
2020-07-09 15:56:06 -0600

    added dashboard template

--- heartbeat_monitor/__init__.py
+++ heartbeat_monitor/__init__.py
@@ -1,34 +1,35 @@
-import sqlite3                    # import sqlite3
-from flask import Flask           # import flask
+import sqlite3                              # import sqlite3
+from flask import Flask, render_template    # import flask and render_template
 
 # Create an app instance
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
+    if test_config is not None:
         app.config.update(test_config)
     
     # Endpoint "/"
     @app.route("/")
-    # def get_db_connection():
-    #        conn = sqlite3.connect('database.db')
-    #         conn.row_factory = sqlite3.Row
-    #         return conn
     def test_db():      # Testing the database
-        # conn = get_db_connection()
-        
         # Establish database connection
         conn = sqlite3.connect('database.db')
         conn.row_factory = sqlite3.Row
+
+        # Get users
         users = conn.execute('SELECT * FROM users').fetchall()
+
+        # Get sensors
         sensors = conn.execute('SELECT * FROM sensors').fetchall()
+
+        # Get heartbeat_readings
         heartbeat_readings = conn.execute('SELECT * FROM heartbeat_readings').fetchall()
+        
         conn.close()
-        print(heartbeat_readings[0]["user_id"])
-        return 'hello'
+        print(heartbeat_readings[0]['user_id'])
+        return "hello"
+    
+    @app.route('/dashboard')
+    def dashboard():
+        return render_template('dashboard.html')
 
     return app
